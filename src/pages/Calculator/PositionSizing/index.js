1c8b3d5feba8e55b317b84e3866220036a9299c0
Watcharagorn <firstw927@gmail.com>
2022-05-04 17:00:36 +0000

    Feat: summary page

--- src/pages/Calculator/PositionSizing/index.js
+++ src/pages/Calculator/PositionSizing/index.js
@@ -85,7 +85,7 @@ function PositionSizing() {
     // eslint-disable-next-line
     const value = e.target.value;
     const newEntryPrice = [...entryPrice];
-    newEntryPrice[index] = parseInt(value, 10);
+    newEntryPrice[index] = value;
     setEntryPrice(newEntryPrice);
   };
 
@@ -147,87 +147,197 @@ function PositionSizing() {
           ml={{ xs: "auto", lg: "auto" }}
           mr={{ xs: "auto", lg: "unset" }}
         >
-          <MKBox
-            bgColor="white"
-            borderRadius="xl"
-            shadow="lg"
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            mt={{ xs: 20 }}
-            mb={{ xs: 1, sm: 2, md: 20 }}
-            mx={3}
+          <Grid
+            id="setupForm"
+            item
+            xs={12}
+            ml={{ xs: "auto", lg: "auto" }}
+            mr={{ xs: "auto", lg: "auto" }}
           >
             <MKBox
-              variant="gradient"
-              bgColor="info"
-              coloredShadow="info"
-              borderRadius="lg"
-              p={2}
-              mx={2}
-              mt={-3}
+              bgColor="white"
+              borderRadius="xl"
+              shadow="lg"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              mt={{ xs: 20 }}
+              mb={1}
+              mx={3}
             >
-              <MKTypography variant="h5" color="white">
-                Position Sizing Calculator
-              </MKTypography>
-            </MKBox>
-            <MKBox p={3}>
-              <MKBox width="100%" component="form" method="post" autocomplete="off">
-                <Grid container item xs={12} spacing={3}>
-                  <Grid item xs={12}>
-                    <MKInput
-                      variant="standard"
-                      label="Wallet's Balance"
-                      InputLabelProps={{ shrink: true }}
-                      type="number"
-                      onChange={walletBalanceChangeHandler}
-                      value={walletBalance}
-                      fullWidth
-                    />
+              <MKBox
+                variant="gradient"
+                bgColor="info"
+                coloredShadow="info"
+                borderRadius="lg"
+                p={2}
+                mx={2}
+                mt={-3}
+              >
+                <MKTypography variant="h5" color="white">
+                  Position Sizing Calculator
+                </MKTypography>
+              </MKBox>
+              <MKBox p={3}>
+                <MKBox width="100%" component="form" method="post" autocomplete="off">
+                  <Grid container item xs={12} spacing={3}>
+                    <Grid item xs={12}>
+                      <MKInput
+                        variant="standard"
+                        label="Wallet's Balance"
+                        InputLabelProps={{ shrink: true }}
+                        type="number"
+                        onChange={walletBalanceChangeHandler}
+                        value={walletBalance}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={6} md={6}>
+                      <MKInput
+                        type="number"
+                        variant="standard"
+                        label="Risk %"
+                        placeholder="1 ~ 99"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={riskPercentChangeHandler}
+                        value={riskPercent}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={6} md={6}>
+                      ~ {riskAmount} USD
+                    </Grid>
+                    <Grid item xs={4}>
+                      <MKInput
+                        variant="standard"
+                        color="success"
+                        label="TP*"
+                        InputLabelProps={{ shrink: true }}
+                        type="number"
+                        onChange={tpPriceChangeHandler}
+                        value={tpPrice}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <MKInput
+                        variant="standard"
+                        color="error"
+                        label="SL**"
+                        InputLabelProps={{ shrink: true }}
+                        type="number"
+                        onChange={slPriceChangeHandler}
+                        value={slPrice}
+                        fullWidth
+                      />
+                    </Grid>
                   </Grid>
-                  <Grid item xs={6} md={6}>
-                    <MKInput
-                      type="number"
-                      variant="standard"
-                      label="Risk %"
-                      placeholder="1 ~ 99"
-                      InputLabelProps={{ shrink: true }}
-                      onChange={riskPercentChangeHandler}
-                      value={riskPercent}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={6} md={6}>
-                    ~ {riskAmount} USD
-                  </Grid>
-                  <Grid item xs={4}>
-                    <MKInput
-                      variant="standard"
-                      color="success"
-                      label="TP*"
-                      InputLabelProps={{ shrink: true }}
-                      type="number"
-                      onChange={tpPriceChangeHandler}
-                      value={tpPrice}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={4}>
-                    <MKInput
-                      variant="standard"
-                      color="error"
-                      label="SL**"
-                      InputLabelProps={{ shrink: true }}
-                      type="number"
-                      onChange={slPriceChangeHandler}
-                      value={slPrice}
-                      fullWidth
-                    />
-                  </Grid>
-                </Grid>
+                </MKBox>
               </MKBox>
             </MKBox>
-          </MKBox>
+          </Grid>
+          <Grid
+            id="orderForm"
+            item
+            xs={12}
+            ml={{ xs: "auto", lg: "auto" }}
+            mr={{ xs: "auto", lg: "auto" }}
+          >
+            <MKBox
+              bgColor="white"
+              borderRadius="xl"
+              shadow="lg"
+              display="flex"
+              flexDirection="column"
+              mt={1}
+              mb={{ xs: 1, sm: 2, md: 20 }}
+              mx={3}
+              p={3}
+            >
+              <Grid container item xs={12}>
+                <Grid container item xs={12} spacing={1}>
+                  {entryPrice.map((value, index) => {
+                    const positionSize = calPositionSize(value);
+                    const elemKey = `sizeResult-${index}`;
+                    return (
+                      <Grid
+                        container
+                        item
+                        className="sizeResultContainer"
+                        key={elemKey}
+                        xs={12}
+                        spacing={1}
+                        justifyContent="flex-start"
+                        alignItems="center"
+                      >
+                        <Grid item xs={4}>
+                          <MKInput
+                            variant="standard"
+                            label={`Entry Point #${index + 1}`}
+                            InputLabelProps={{ shrink: true }}
+                            type="number"
+                            onChange={(e) => {
+                              entryPriceChangeHandler(e, index);
+                            }}
+                            value={value}
+                            fullWidth
+                          />
+                        </Grid>
+                        <Grid container item xs={8} spacing={0.5}>
+                          <Grid item>
+                            <span>
+                              <small>~ Size:</small>
+                            </span>
+                            <b style={{ marginLeft: 5, marginRight: 10 }}>{positionSize}</b>
+                          </Grid>
+                          <Grid item>
+                            <Grid container spacing={0.5}>
+                              <Grid item>
+                                <CopyToClipboard text={positionSize}>
+                                  <MKButton
+                                    variant="gradient"
+                                    color={copyStatus[index] ? "success" : "warning"}
+                                    size="small"
+                                    onClick={() => copySize(index)}
+                                  >
+                                    <MKBox
+                                      color="white"
+                                      className={copyStatus[index] ? "fa fa-check" : "fas fa-copy"}
+                                    />
+                                  </MKButton>
+                                </CopyToClipboard>
+                              </Grid>
+                              <Grid item>
+                                <MKButton
+                                  variant="gradient"
+                                  color="error"
+                                  size="small"
+                                  onClick={() => removeOrder(index)}
+                                >
+                                  <Icon>delete</Icon>
+                                </MKButton>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    );
+                  })}
+                  <Grid item xs={12}>
+                    <MKButton
+                      size="small"
+                      color="success"
+                      onClick={() => addOrder(true)}
+                      sx={{ my: 2 }}
+                      circular
+                    >
+                      <Icon>add</Icon>&nbsp; New order
+                    </MKButton>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </MKBox>
+          </Grid>
         </Grid>
         <Grid
           item
@@ -237,100 +347,46 @@ function PositionSizing() {
           ml={{ xs: "auto", lg: "unset" }}
           mr={{ xs: "auto", lg: "auto" }}
         >
-          <MKBox
-            bgColor="white"
-            borderRadius="xl"
-            shadow="lg"
-            display="flex"
-            flexDirection="column"
-            mt={{ xs: 1, sm: 2, md: 20 }}
-            mb={{ xs: 1, sm: 2, md: 20 }}
-            mx={3}
-            p={3}
+          <Grid
+            id="summaryForm"
+            item
+            xs={12}
+            ml={{ xs: "auto", lg: "auto" }}
+            mr={{ xs: "auto", lg: "auto" }}
           >
-            <Grid container item xs={12}>
-              <Grid container item xs={12} spacing={1}>
-                {entryPrice.map((value, index) => {
-                  const positionSize = calPositionSize(value);
-                  const elemKey = `sizeResult-${index}`;
-                  return (
-                    <Grid
-                      container
-                      item
-                      className="sizeResultContainer"
-                      key={elemKey}
-                      xs={12}
-                      spacing={1}
-                      justifyContent="flex-start"
-                      alignItems="center"
-                    >
-                      <Grid item xs={4}>
-                        <MKInput
-                          variant="standard"
-                          label={`Entry Point #${index + 1}`}
-                          InputLabelProps={{ shrink: true }}
-                          type="number"
-                          onChange={(e) => {
-                            entryPriceChangeHandler(e, index);
-                          }}
-                          value={value}
-                          fullWidth
-                        />
-                      </Grid>
-                      <Grid container item xs={8} spacing={0.5}>
-                        <Grid item>
-                          <span>
-                            <small>~ Size:</small>
-                          </span>
-                          <b style={{ marginLeft: 5, marginRight: 10 }}>{positionSize}</b>
-                        </Grid>
-                        <Grid item>
-                          <Grid container spacing={0.5}>
-                            <Grid item>
-                              <CopyToClipboard text={positionSize}>
-                                <MKButton
-                                  variant="gradient"
-                                  color={copyStatus[index] ? "success" : "warning"}
-                                  size="small"
-                                  onClick={() => copySize(index)}
-                                >
-                                  <MKBox
-                                    color="white"
-                                    className={copyStatus[index] ? "fa fa-check" : "fas fa-copy"}
-                                  />
-                                </MKButton>
-                              </CopyToClipboard>
-                            </Grid>
-                            <Grid item>
-                              <MKButton
-                                variant="gradient"
-                                color="error"
-                                size="small"
-                                onClick={() => removeOrder(index)}
-                              >
-                                <Icon>delete</Icon>
-                              </MKButton>
-                            </Grid>
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  );
-                })}
-                <Grid item xs={12}>
-                  <MKButton
-                    size="small"
-                    color="success"
-                    onClick={() => addOrder(true)}
-                    sx={{ my: 2 }}
-                    circular
-                  >
-                    <Icon>add</Icon>&nbsp; New order
-                  </MKButton>
+            <MKBox
+              bgColor="white"
+              borderRadius="xl"
+              shadow="lg"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              mt={{ xs: 20 }}
+              mb={{ xs: 1, sm: 2, md: 20 }}
+              mx={3}
+            >
+              <MKBox
+                variant="gradient"
+                bgColor="info"
+                coloredShadow="info"
+                borderRadius="lg"
+                p={2}
+                mx={2}
+                mt={-3}
+              >
+                <MKTypography variant="h5" color="white">
+                  Summary
+                </MKTypography>
+              </MKBox>
+              <MKBox p={3}>
+                <Grid container>
+                  <Grid item>
+                    <h3>Total Profit: XXX</h3>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Grid>
-          </MKBox>
+              </MKBox>
+            </MKBox>
+          </Grid>
         </Grid>
       </Grid>
     </>
